1536c9b91e0b38b5abb3cc37598019f773ef36c1
ad1992 <aakansha1216@gmail.com>
2021-10-18 19:12:39 +0530

    feat: track user state with visibilityAPI

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,8 @@
 export enum EVENT {
   BEFORE_UNLOAD = "beforeunload",
   UNLOAD = "unload",
+  VISIBILITY_CHANGE = "visibilitychange",
+  POINTER_MOVE = "pointermove",
 }
 
 export const WEBEX_URL =
@@ -9,3 +11,23 @@ export const WEBEX_URL =
 export const ENV = {
   DEVELOPMENT: "development",
 };
+
+// Report a user inactive after IDLE_THRESHOLD milliseconds
+export const IDLE_THRESHOLD = 60_000;
+// Report a user active each ACTIVE_THRESHOLD milliseconds
+export const ACTIVE_THRESHOLD = 3_000;
+
+export const SYNC_FULL_SCENE_INTERVAL_MS = 20000;
+export const INITIAL_SCENE_UPDATE_TIMEOUT = 5000;
+
+export const BROADCAST = {
+  SERVER_VOLATILE: "server-volatile-broadcast",
+  SERVER: "server-broadcast",
+};
+
+export enum SCENE {
+  INIT = "SCENE_INIT",
+  UPDATE = "SCENE_UPDATE",
+}
+
+export const APP_NAME = "Excalidraw";
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,3 @@
-
 export enum UserIdleState {
   ACTIVE = "active",
   AWAY = "away",
